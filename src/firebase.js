b926230c6190eccd2b80151392fe99ed291ba619
Michael <jtmikee@gmail.com>
2021-03-05 14:33:40 -0500

    formatted app and added firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from './firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEcsKx99XGuSH9sExIFGd1td0uRrTyu4Q",
+    authDomain: "project-three-myplace.firebaseapp.com",
+    projectId: "project-three-myplace",
+    storageBucket: "project-three-myplace.appspot.com",
+    messagingSenderId: "829252832191",
+    appId: "1:829252832191:web:5bcaeffa01e23e26982f0a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
