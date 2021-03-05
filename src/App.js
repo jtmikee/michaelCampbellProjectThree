d3efd74e79669d4b11fc7a7b4d9c16913dfed0f0
Michael <jtmikee@gmail.com>
2021-03-05 15:36:44 -0500

    comments section started

--- src/App.js
+++ src/App.js
@@ -3,8 +3,62 @@ import firebase from './firebase.js'
 import { useState, useEffect } from 'react';
 
 function App() {
+
+  //setup empty comments array in state
+  const [ commentsArray, setCommentsArray ] = useState([]);
+
+  //setup state for the text input for the comments 
+  const [ textInput, setTextInput ] = useState("");
+
+  //setup useEffect for database
+  useEffect(() => {
+    const databaseRef = firebase.database().ref();
+
+    databaseRef.on('value', (data) => {
+
+      const commentsData = data.val();
+
+      const commentsEmptyArray = [];
+
+      for (let commentsKey in commentsData) {
+        commentsEmptyArray.push({
+          key: commentsKey,
+          userComment: commentsData[commentsKey]
+        })
+      }
+
+      setCommentsArray(commentsEmptyArray);
+
+    })
+
+  }, []);
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
+
+
+      {/* map through commentsArray */}
+      <ul className="commentSection">
+        {
+          commentsArray.map((comment) => {
+            return (
+              <li key={comment.commentsKey}>
+                {comment.userComment}
+              </li>
+            )
+          })
+        }
+
+      </ul>
       
     </div>
   );
